c092a5d0d31185c353c6c4ec43a4b23a9030b7e0
jacobsall <jacob.e.s.nilsson@gmail.com>
2022-12-04 15:57:44 +0100

    remove numpy dependency

--- 1.py
+++ 1.py
@@ -1,17 +1,16 @@
 import sys
-import numpy as np
 
 f = open("inputs/1.txt")
 data = f.read()
 
-grouped = [np.sum([int(num) for num in group.split('\n')]) for group in data.split('\n\n')]
+grouped = [sum([int(num) for num in group.split('\n')]) for group in data.split('\n\n')]
 
 sortedList = list(reversed(sorted(grouped)))
 
 part1 = sortedList[0]
 print(f"part 1: {part1}")
 
-part2 = np.sum(sortedList[0:3])
+part2 = sum(sortedList[0:3])
 print(f"part 2: {part2}")
 
 
