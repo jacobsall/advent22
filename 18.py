6224bded2d466bd674ca9323c9a59c7520db49ab
Jacob Säll Nilsson <jacob.e.s.nilsson@gmail.com>
2022-12-25 14:55:12 +0100

    day 18 done

---
+++ 18.py
@@ -0,0 +1,39 @@
+import numpy as np
+from scipy.ndimage import binary_fill_holes
+
+def manhattan(a, b):
+    x1,y1,z1 = a
+    x2,y2,z2 = b
+    return abs(x1-x2)+abs(y1-y2)+abs(z1-z2)
+
+def neighbors(point,grid):
+  x,y,z = point
+  nbs = []
+  nbs.extend([(x-1,y,z),(x+1,y,z)])
+  nbs.extend([(x,y-1,z),(x,y+1,z)])
+  nbs.extend([(x,y,z-1),(x,y,z+1)])
+  nbs = [grid[a][b][c] for a,b,c in nbs]
+  return sum(nbs)
+
+f = open("inputs/18.txt")
+data = f.read().split('\n')
+
+PART2 = True
+
+SIZE = 24
+grid = np.zeros((SIZE,SIZE,SIZE)) # to inlude "-1" and "23"
+newgrid = np.zeros((SIZE,SIZE,SIZE))
+
+for line in data:
+  x,y,z = map(int, line.split(','))
+  grid[x+1][y+1][z+1] = 1
+
+if PART2:
+  grid = binary_fill_holes(grid)
+
+total = 0
+for line in data:
+  x,y,z = map(int, line.split(','))
+  newgrid[x+1][y+1][z+1] = 6 - neighbors((x+1,y+1,z+1),grid)
+
+print(np.sum(newgrid))
